b2e7057bf7ed2721e20e97c72f4dabf3ed269f19
gabrisousa <gabrielsousaferreira184@gmail.com>
2025-09-22 17:26:58 -0300

    ajus

--- src/Styles.js
+++ src/Styles.js
@@ -215,12 +215,10 @@ export const FotoImg = styled.img`
   box-shadow: 4px 4px 4px 4px black;
 
   @media (max-width: 768px) {
-    height: 200px;
+    height: 400px;
   }
 
-  @media (max-width: 480px) {
-    height: 160px;
-  }
+  
 `;
 
 
